8ae7bc61d9e49db9a96b243fe3e449b3af8b63ef
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2023-01-15 09:00:36 +0530

    click btn js

---
+++ CSS Challenges/Motivational Thoughts/main.js
@@ -0,0 +1,13 @@
+var active = 1;
+$(".button").bind("click", function () {
+  $(".card:nth-child(" + active + ")")
+    .removeClass("active")
+    .addClass("inactive");
+  if (active == 3) {
+    active = 0;
+  }
+  active++;
+  $(".card:nth-child(" + active + ")")
+    .removeClass("inactive")
+    .addClass("active");
+});
